51bcf2813ea19b2a7ece59fd4dd109897f2ac50c
Sunil Shedge <51407211+surya88i@users.noreply.github.com>
2020-06-25 10:11:10 +0530

    Add files via upload

---
+++ lib/screen/theme/theme.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+Color primaryGreen=Color(0xFF333945);
+List<BoxShadow> shadowList=[
+  BoxShadow(
+    blurRadius: 30,
+    color: Colors.grey[300],
+    offset: Offset(0, 30),   
+  ),
+];
+List<Map> categories=[
+  {'name':'Hamburger','iconPath':'assets/images/burger.png'},
+  {'name':'Hamburger','iconPath':'assets/images/burger.png'},
+  {'name':'Hamburger','iconPath':'assets/images/burger.png'},
+  {'name':'Hamburger','iconPath':'assets/images/burger.png'},
+  {'name':'Hamburger','iconPath':'assets/images/burger.png'},
+  {'name':'Hamburger','iconPath':'assets/images/burger.png'},
+  {'name':'Hamburger','iconPath':'assets/images/burger.png'},
+  {'name':'Hamburger','iconPath':'assets/images/burger.png'},
+  {'name':'Hamburger','iconPath':'assets/images/burger.png'},
+  {'name':'Hamburger','iconPath':'assets/images/burger.png'},
+
+];
+List<Map> drawerItems=[
+  {
+    'title':'Home',
+    'icon':Icons.home,
+  },
+  {
+    'title':'Home',
+    'icon':Icons.home,
+  },
+  {
+    'title':'Home',
+    'icon':Icons.home,
+  },
+  {
+    'title':'Home',
+    'icon':Icons.home,
+  },
+];
